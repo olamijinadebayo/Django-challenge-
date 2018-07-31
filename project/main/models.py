4cf29071b428f9c3882ebb81fe6f7916fd67137a
olamijinadebayo <consultolamijin@gmail.com>
2018-07-31 14:04:58 +0100

    This will add a model class person which will be instantiated each time someone creates or adds a name and email

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name= models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 50,unique = True)
+    timecreated = models.DateTimeField(auto_now=True)
