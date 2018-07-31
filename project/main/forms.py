a707dcbd8394c943a2fb2f16775e08d5baf82cbc
olamijinadebayo <consultolamijin@gmail.com>
2018-07-31 14:25:40 +0100

    This will add a forms.py file and also declare the Person form where the users can enter their name and email

---
+++ project/main/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.core.validators import validate_email
+
+from .models import Users
+
+class PersonForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if Person.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email has been used already')
+        return email
